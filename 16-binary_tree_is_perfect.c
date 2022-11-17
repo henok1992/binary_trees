abdf050586188d04160ef27d1aeca0a2c0cd175e
henok1992 <henokgithub@gmail.com>
2022-11-17 06:30:58 -0800

    WHFKE

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,38 +1,72 @@
-#include "binary_trees.h"
+ #include "binary_trees.h"
+
 /**
- * binary_tree_is_full - check full or not
- * @tree: tree of binary
- * Return: 0 if not full or 1 for full
-*/
-bool is_perfect(const binary_tree_t *root, int d, int level);
-int depth(const binary_tree_t *node);
-int  binary_tree_is_perfect(const binary_tree_t *tree)
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: height of the binary tree or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-    int d;
-    if (tree == NULL)
-    return (0);
-d = depth(tree);
-  return is_perfect(tree, d, 0);
+	size_t left_height, right_height, max;
+
+	if (!tree)
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	max = left_height >= right_height ? left_height : right_height;
+
+	return (max + 1);
 }
 
-int depth(const binary_tree_t *node) {
-  int d = 0;
-  while (node != NULL) {
-    d++;
-    node = node->left;
-  }
-  return d;
+/**
+ * binary_tree_balance -  measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure
+ * the balance factor
+ * Return: (0) if tree is NULL else binary tree balance factor
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (!tree)
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (left_height - right_height);
 }
 
-bool is_perfect(const binary_tree_t *root, int d, int level) {
-  if (root == NULL)
-    return true;
-  if (root->left == NULL && root->right == NULL)
-    return (d == level + 1);
+/**
+ * binary_tree_is_leaf - checks if a certain node is a leaf node
+ * @node: node to check
+ * Return: nothing
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	if (!node->left && !node->right && node)
+		return (1);
+	return (0);
+}
 
-  if (root->left == NULL || root->right == NULL)
-    return false;
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ * Return: (0) if tree is perfect else (0)
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (binary_tree_is_leaf(tree))
+		return (1);
 
-  return is_perfect(root->left, d, level + 1) &&
-       is_perfect(root->right, d, level + 1);
+	if (binary_tree_balance(tree) == 0)
+	{
+		if (binary_tree_is_perfect(tree->left) &&
+		    binary_tree_is_perfect(tree->right))
+			return (1);
+	}
+	return (0);
 }
